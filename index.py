51201b746d1155862088524e18a3f1aa3ec802ac
samir benzada <samirbenzada@gmail.com>
2020-07-03 15:09:06 -0400

    update

--- index.py
+++ index.py
@@ -1,10 +1,23 @@
 ## read a file and push every word to a list
 
-text = open("Shakespeare.txt", "r")
+# text = open("Shakespeare.txt", "r")
 
 # text = " This is the 100th Etext file presented by Project Gutenberg, and is presented in cooperation with World Library, Inc., from their Library of the Future and Shakespeare CDROMS.  Project Gutenberg often releases Etexts that are NOT placed in the Public Domain!! "
+import re
+with open('test.txt', 'r') as myfile:
+  text = myfile.read()
 
-x = text.split()
+# re.sub(r'[^\w\s]','',text)
+# print(text)
+
+# x = text.split()
+
+ 
+
+x = re.split(' |\n|:|"|;|\.|\'|!|\?|,|>|<', text)
+
+# print(x)
+
 newstr = []
 
 for word in x:
