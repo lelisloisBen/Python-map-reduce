624f61fd3b7e5f35f9d98770086f7544ab30843a
samir benzada <samirbenzada@gmail.com>
2020-07-03 13:54:41 -0400

    update

--- index.py
+++ index.py
@@ -1,8 +1,8 @@
 ## read a file and push every word to a list
 
-#f = open("Shakespeare.txt", "r")
+text = open("Shakespeare.txt", "r")
 
-text = " This is the 100th Etext file presented by Project Gutenberg, and is presented in cooperation with World Library, Inc., from their Library of the Future and Shakespeare CDROMS.  Project Gutenberg often releases Etexts that are NOT placed in the Public Domain!! "
+# text = " This is the 100th Etext file presented by Project Gutenberg, and is presented in cooperation with World Library, Inc., from their Library of the Future and Shakespeare CDROMS.  Project Gutenberg often releases Etexts that are NOT placed in the Public Domain!! "
 
 x = text.split()
 newstr = []
