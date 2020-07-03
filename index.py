07ac1569a35d4ba4205db9dca8fa25ad20e84644
samir benzada <samirbenzada@gmail.com>
2020-07-03 15:14:01 -0400

    update

--- index.py
+++ index.py
@@ -1,28 +1,22 @@
-## read a file and push every word to a list
+## Map reduce with python
 
-# text = open("Shakespeare.txt", "r")
-
-# text = " This is the 100th Etext file presented by Project Gutenberg, and is presented in cooperation with World Library, Inc., from their Library of the Future and Shakespeare CDROMS.  Project Gutenberg often releases Etexts that are NOT placed in the Public Domain!! "
 import re
+
+# read the file
 with open('test.txt', 'r') as myfile:
   text = myfile.read()
 
-# re.sub(r'[^\w\s]','',text)
-# print(text)
-
-# x = text.split()
-
- 
-
+# removing ponctuation
 x = re.split(' |\n|:|"|;|\.|\'|!|\?|,|>|<', text)
 
-# print(x)
-
+# create empty list
 newstr = []
 
+# looping and append every words to the new list
 for word in x:
     if word not in newstr:
         newstr.append(word)
 
+# counting the frequency of each word and print them
 for word in range(0, len(newstr)):
     print(newstr[word], text.count(newstr[word]))
